e882a994025c6a5689a2c952de92e3b9cea0796c
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2024-05-10 11:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ st.header("Extract Information from PDF")
 uploaded_file = st.file_uploader("choose a pdf file...", type=["pdf"])
 
 if uploaded_file is not None:
-# Display uploaded file details
-st.subheader("Uploaded PDF File Details:")
-st.write("File Name:", uploaded_file.name)
-st.write("File Size:", len(uploaded_file.getvalue()), "bytes")
+  # Display uploaded file details
+  st.subheader("Uploaded PDF File Details:")
+  st.write("File Name:", uploaded_file.name)
+  st.write("File Size:", len(uploaded_file.getvalue()), "bytes")
 
 input=st.text_input("Ask Question: ",key="input")
 submit = st.button("Tell me about the PDF")
 #if submit button is clicked,
 if submit:
-st.subheader("The response is..")
-st.write('response') # st.write(response) if answer stored in variable
+  st.subheader("The response is..")
+  st.write('response') # st.write(response) if answer stored in variable
